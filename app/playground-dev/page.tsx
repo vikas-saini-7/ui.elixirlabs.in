4fd8131bcf90f0654d23e7f8ea7c3448f6fe9959
vikas-saini-7 <vikas289888@gmail.com>
2026-02-20 21:49:15 +0530

    feat: removed extra files

--- app/playground-dev/page.tsx
+++ app/playground-dev/page.tsx
@@ -1,14 +1,10 @@
 import { notFound } from "next/navigation";
-import Playground from "@/playground/index";
+// import Playground from "@/playground/index";
 
 export default function PlaygroundPage() {
   if (process.env.NEXT_PUBLIC_ENABLE_PLAYGROUND !== "true") {
     notFound();
   }
 
-  return (
-    <div>
-      <Playground />
-    </div>
-  );
+  return <div>{/* <Playground /> */}</div>;
 }
